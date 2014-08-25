edf74982c64c912a04811367796f8f7bc4a5ec82
pikafu <siyi.jfu@gmail.com>
2014-08-24 20:45:42 -0400

    Works after LocalMidi was changed.

--- TestMidi/playback_test.py
+++ TestMidi/playback_test.py
@@ -1,18 +1,17 @@
 """ Verifies local MIDI receive/transmit """
 __author__ = 'John Fu, 2014.'
 
-from LocalMidi.LocalMidi import KeyboardMidi
+from MidiHandler.KeyboardMidi import LocalMidi
 
 if __name__ == "__main__":
-    localmidi = KeyboardMidi()
+    localmidi = LocalMidi()
     localmidi.setup_local_midi()
 
     while True:
         msg, delta_time = localmidi.MIDI_IN.get_message()
         if msg:
             print(msg)
-            msg_type, msg_max_length = localmidi.get_msg_info(msg[0])
-
+            msg_type = localmidi.get_msg_type(msg[0])
             if msg_type == localmidi.ON_OFF:
                 localmidi.MIDI_OUT.send_message([msg[0], msg[1] + 3, msg[2]])
             else:
