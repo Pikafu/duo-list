8b5545199567d95e0af5db6eee942008b2341e8f
pikafu <siyi.jfu@gmail.com>
2014-09-01 22:08:45 -0400

    Added files for asyncio

--- TestMidi/echo_client_asyncio.py
+++ TestMidi/echo_client_asyncio.py
@@ -2,7 +2,14 @@ __author__ = 'John'
 import asyncio
 
 class EchoClient(asyncio.Protocol):
-    message = "hey you, yea you, I'm talking to you"
+
+    def __init__(self):
+        # IP of the server instance this code is running on
+        #AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
+        selfHOST = socket.gethostbyname(AMAZON_DNS)
+        #self.HOST = "localhost"
+        self.PORT = 3001              # The same port as used by the server
+        self.message = "hey you, yea you, I'm talking to you"
 
     def connection_made(self, transport):
         transport.write(self.message.encode())
@@ -15,8 +22,9 @@ class EchoClient(asyncio.Protocol):
         print('server close the connection')
         asyncio.get_event_loop().stop()
 
+c = EchoClient()
 loop = asyncio.get_event_loop()
-coro = loop.create_connection(EchoClient, 'localhost', 3001)
+coro = loop.create_connection(c, c.HOST, c.PORT)
 loop.run_until_complete(coro)
 loop.run_forever()
 loop.close()
--- TestMidi/echo_server_asyncio.py
+++ TestMidi/echo_server_asyncio.py
@@ -2,6 +2,15 @@ __author__ = 'John'
 import asyncio
 
 class EchoServer(asyncio.Protocol):
+
+    def __init__(self):
+        # IP of the server instance this code is running on
+        #AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
+        #HOST = socket.gethostbyname(AMAZON_DNS)
+        self.HOST = "localhost"
+        self.PORT = 3001              # The same port as used by the server
+        self.transport = ""
+
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
         print('connection from {}'.format(peername))
@@ -10,12 +19,12 @@ class EchoServer(asyncio.Protocol):
     def data_received(self, data):
         print('data received {}'.format(data.decode()))
         self.transport.write(data)
-
         #close the socket
         self.transport.close()
 
+e = EchoServer()
 loop = asyncio.get_event_loop()
-coro = loop.create_server(EchoServer, 'localhost', 3001)
+coro = loop.create_server(e, e.HOST, e.PORT)
 server = loop.run_until_complete(coro)
 print('serving on {}'.format(server.sockets[0].getsockname()))
 
