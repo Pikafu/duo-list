d187ddd62e156129ac50ce7a01c3df6b156e2525
pikafu <siyi.jfu@gmail.com>
2014-09-08 22:53:47 -0400

    Made it work with multiple clients ahhhhh.

--- NetworkMidi/MidiServer.py
+++ NetworkMidi/MidiServer.py
@@ -1,63 +1,60 @@
-import sys, os
+""" IOStream based TCP server, along with connection handler methods.
+    Huge credit goes to
+
+        http://w.gdu.me/wiki/Python/Python_socket.html
+
+    for inspiring this code.
+"""
+__author__ = 'John Fu, 2014.'
+
+import os
+import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from MidiHandler.KeyboardMidi import LocalMidi
+
 from NetworkMidi.EC2 import EC2Server
-import errno
-import functools
-import socket
-from tornado import ioloop, iostream
- 
- 
-class MidiConnection(object):
-    def __init__(self, connection):
-        self.stream = iostream.IOStream(connection)
+from tornado.tcpserver import TCPServer
+from tornado.ioloop import IOLoop
+
+
+class MidiTCPServer(TCPServer):
+    """ Override handle_stream to initialize a connection. """
+    def handle_stream(self, stream, address):
+        print("New connection from:", address, stream)
+        MidiConnectionHandler(stream, address)
+        print(len(MidiConnectionHandler.clients), " clients connected")
+
+
+class MidiConnectionHandler(object):
+    """ Class for handling connections to the server. """
+    clients = set()
+
+    def __init__(self, stream, address):
+        MidiConnectionHandler.clients.add(self)
+        self._stream = stream
+        self._address = address
+        self._stream.set_close_callback(self.on_close)
         self._read()
- 
+        print("A new user has entered the chat room: ", address)
+
     def _read(self):
-        self.stream.read_bytes(num_bytes=16, partial=True, callback=self._eol_callback)
+        self._stream.read_until(b'\n', callback=self._broadcast)
  
-    def _eol_callback(self, data):
-        self.handle_data(data)
-
-
-class MidiClient(MidiConnection):
-    """Put your app logic here"""
-    def handle_data(self, data):
-        print(data)
-        self.stream.write(data)
+    def _broadcast(self, data):
+        print("User played:", data[:-1], self._address)
+        for conn in MidiConnectionHandler.clients:
+            conn.send_message(data)
         self._read()
-
-
-def setup_socket():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.setblocking(0)
-    sock.bind((EC2Server.HOST, EC2Server.PORT))
-    sock.listen(1)
-    return sock
-
-
-def connection_ready(sock, fd, events):
-    while True:
-        try:
-            connection, address = sock.accept()
-        except OSError as e:
-            if e.errno not in (errno.EWOULDBLOCK, errno.EAGAIN):
-                raise
-            return
-        else:
-            connection.setblocking(0)
-            MidiClient(connection)
  
+    def send_message(self, data):
+        self._stream.write(data)
+ 
+    def on_close(self):
+        print("A user has left .", self._address)
+        MidiConnectionHandler.clients.remove(self)
+
  
 if __name__ == '__main__':
-    sock = setup_socket()
-    io_loop = ioloop.IOLoop.instance()
-    callback = functools.partial(connection_ready, sock)
-    io_loop.add_handler(sock.fileno(), callback, io_loop.READ)
-    
-    try:
-        io_loop.start()
-    except KeyboardInterrupt:
-        io_loop.stop()
-        print("exited cleanly")
+    print("Server start ...")
+    server = MidiTCPServer()
+    server.listen(EC2Server.PORT, EC2Server.HOST)
+    IOLoop.instance().start()
