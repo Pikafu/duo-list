8ec66fc856ff2e0ec83e4aa02cd9b2ccc72d7393
pikafu <siyi.jfu@gmail.com>
2014-09-10 00:05:24 -0400

    Added coroutines and verified receive and send for a single client. Still need to broadcast to all clients.

--- NetworkMidi/MidiServer.py
+++ NetworkMidi/MidiServer.py
@@ -1,7 +1,9 @@
 """ IOStream based TCP server, along with connection handler methods.
-    Huge credit goes to
+    Uses co-routines as opposed to callbacks for simplicity of code.
+    Huge thank you and credit goes to
 
-        http://w.gdu.me/wiki/Python/Python_socket.html
+        XStar - http://w.gdu.me/wiki/Python/Python_socket.html
+        picomancer - https://github.com/picomancer/echoserver/blob/d590b375304ced9bc0609b85270491f58eb6d788/echoserver.py
 
     for inspiring this code.
 """
@@ -14,14 +16,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from NetworkMidi.EC2 import EC2Server
 from tornado.tcpserver import TCPServer
 from tornado.ioloop import IOLoop
+from tornado.gen import coroutine
+from tornado.iostream import StreamClosedError
 
 
 class MidiTCPServer(TCPServer):
     """ TCP server sets up an IOStream. Override handle_stream
         and pass these onto the connection handler. """
+    @coroutine
     def handle_stream(self, stream, address):
         print("New connection from:", address, stream)
-        MidiConnectionHandler(stream, address)
+        conn = MidiConnectionHandler(stream, address)
+        yield conn.on_connect()
         print(len(MidiConnectionHandler.clients), " clients connected")
 
 
@@ -34,21 +40,25 @@ class MidiConnectionHandler(object):
         self._stream = stream
         self._address = address
         self._stream.set_close_callback(self.on_close)
-        self._read()
-        print("A new user has entered the chat room: ", address)
+        #self._read()
 
-    def _read(self):
-        self._stream.read_until(b'\n', callback=self._broadcast)
+    @coroutine
+    def on_connect(self):
+        print("A new user has joined from: ", self._address)
+        yield self.broadcast()
+        return
 
-    def send_message(self, data):
-        self._stream.write(data)
+    @coroutine
+    def broadcast(self):
+        try:
+            while True:
+                m = yield self._stream.read_until(b'\n')
+                print(m)
+                yield self._stream.write(m[:-1])
+        except StreamClosedError:
+            pass
+        return
 
-    def _broadcast(self, data):
-        print("User played:", data[:-1], self._address)
-        for conn in MidiConnectionHandler.clients:
-            conn.send_message(data)
-        self._read()
- 
     def on_close(self):
         print("A user has left .", self._address)
         MidiConnectionHandler.clients.remove(self)
