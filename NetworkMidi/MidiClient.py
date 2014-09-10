e7f2a2ad0832636de1f14001a59e6ad27c02d8b3
pikafu <siyi.jfu@gmail.com>
2014-09-10 00:06:37 -0400

    Added coroutines and verified poll functionality. Still cannot receive from MidiServer.

--- NetworkMidi/MidiClient.py
+++ NetworkMidi/MidiClient.py
@@ -8,32 +8,54 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from MidiHandler.KeyboardMidi import LocalMidi
 from NetworkMidi.EC2 import EC2Server
-from tornado.iostream import IOStream
+from tornado.tcpclient import TCPClient
 from tornado.ioloop import IOLoop
+from tornado.iostream import IOStream
+from tornado.gen import coroutine
+from tornado.iostream import StreamClosedError
 
 
-class MidiTCPClient(object):
+class MidiTCPClient():
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+    @coroutine
+    def connect(self):
+        conn = MidiConnectionHandler(self.host, self.port)
+        print("Connected to: ", self.host, self.port)
+        yield conn.start()
+
+
+class MidiConnectionHandler(object):
     def __init__(self, host, port):
         self._host = host
         self._port = port
         self._localmidi = LocalMidi()
-        self.start()
-
-    def start(self):
         self._localmidi.setup_local_midi()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.stream = IOStream(s)
-        self.stream.connect((EC2Server.HOST, EC2Server.PORT), self._poll_from_keyboard)
-        IOLoop.instance().start()
-        #self._read()
-        self._poll_from_keyboard()
+        #IOLoop.instance().start()
+        print("started")
+        return
+        #self.start()
+
+    @coroutine
+    def start(self):
+        s = yield self.stream.connect((EC2Server.HOST, EC2Server.PORT))
+        yield self._poll_from_keyboard(s)
+        yield self.read()
+        #self._poll_from_keyboard()
+        return
 
     # On read:
     # Send to keyboard
     # Poll for midi runs in the background and writes
 
-    def _read(self):
-        self.stream.read_until(b'\n', callback=self._send_to_keyboard)
+    @coroutine
+    def read(self):
+        rx = yield self.stream.read_until(b'\n')
+        print("received from server: ", rx)
 
     def _send_to_keyboard(self, data):
         """ Received data is in bytes so need to convert to rtmidi tuple format. """
@@ -41,17 +63,15 @@ class MidiTCPClient(object):
         self._localmidi.MIDI_OUT_CONN.send_message(m)
         #self._poll_from_keyboard()
 
-    def _poll_from_keyboard(self):
+    @coroutine
+    def _poll_from_keyboard(self, stream):
         """ Polls the connected MIDI device for incoming data, then sends it. """
+        print("polling for midi: ")
         while True:
             msg, delta_time = self._localmidi.MIDI_IN_CONN.get_message()
             if msg is not None and msg[0] is not self._localmidi.SYSEX_MSG:
-                print("sending: ", msg)
-                self._send(msg)
-
-    def _send(self, data):
-        """ Sends data to the stream. """
-        self.stream.write(bytes(data) + '\n'.encode())
+                yield stream.write(bytes(msg) + '\n'.encode())
+                print("sent midi packet")
 
 #def on_headers(data):
 #    print("Received ", data)
@@ -63,5 +83,5 @@ class MidiTCPClient(object):
 
 if __name__ == "__main__":
     m = MidiTCPClient(EC2Server.HOST, EC2Server.PORT)
-    m.start()
-    #IOLoop.instance().start()
+    m.connect()
+    IOLoop.instance().start()
