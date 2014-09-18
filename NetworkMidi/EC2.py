048c4c0d3891b3634bef05cdebafe3f92e5fbdfd
pikafu <siyi.jfu@gmail.com>
2014-09-18 01:12:18 -0400

    Testing remote server now

--- NetworkMidi/EC2.py
+++ NetworkMidi/EC2.py
@@ -5,7 +5,7 @@ import socket
 
 
 class EC2Server:
-    #AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
-    HOST = 'localhost'
-    #HOST = socket.gethostbyname(AMAZON_DNS)
+    AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
+    #HOST = 'localhost'
+    HOST = socket.gethostbyname(AMAZON_DNS)
     PORT = 3001              # Arbitrary high port
