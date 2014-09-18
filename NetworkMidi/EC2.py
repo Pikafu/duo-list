444201d8628091c7526880bf4a911530d5545a59
pikafu <siyi.jfu@gmail.com>
2014-09-17 20:37:06 -0400

    Minor changes.

--- NetworkMidi/EC2.py
+++ NetworkMidi/EC2.py
@@ -5,9 +5,7 @@ import socket
 
 
 class EC2Server:
-    def __init__(self):
-        # IP of the E2C server instance this code is running on
-        #self.HOST = "localhost"
-        self.AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
-        self.HOST = socket.gethostbyname(self.AMAZON_DNS)
-        self.PORT = 3001              # Arbitrary high port
+    #AMAZON_DNS = 'ec2-54-68-7-241.us-west-2.compute.amazonaws.com'
+    HOST = 'localhost'
+    #HOST = socket.gethostbyname(AMAZON_DNS)
+    PORT = 3001              # Arbitrary high port
