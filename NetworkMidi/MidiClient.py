93d11062243033dff3385b3bcc03b663d7aeeb52
pikafu <siyi.jfu@gmail.com>
2014-09-18 23:25:37 -0400

    Client now can send and receive notes from the server, while polling the keyboard. Next up, optimizing so that simultaneously played notes are heard...simultaneously.

--- NetworkMidi/MidiClient.py
+++ NetworkMidi/MidiClient.py
@@ -15,65 +15,73 @@ from tornado.ioloop import IOLoop
 from tornado.iostream import IOStream
 from tornado.gen import coroutine
 from tornado.iostream import StreamClosedError
+from time import sleep
 
 
 class MidiTCPClient():
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.keyboard_listener = Queue()
 
+    @coroutine
     def connect(self):
         conn = MidiConnectionHandler(self.host, self.port)
-        print("Connected to: ", self.host, self.port)
-        conn.start()
-
+        yield conn.start()
 
 class MidiConnectionHandler(object):
     def __init__(self, host, port):
         self._host = host
         self._port = port
+        self.keyboard_listener = Queue()
+        self.server_listener = Queue()
         self._localmidi = LocalMidi()
         self._localmidi.setup_local_midi()
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.stream = IOStream(s)
         self.stream.set_close_callback(self.on_disconnect)
-        #IOLoop.instance().start()
-        print("started")
-        return
-        #self.start()
 
+    @coroutine
     def start(self):
-        self.stream.connect((EC2Server.HOST, EC2Server.PORT))
-        print("In start")
-        Thread(target=self._send_to_keyboard, args=()).start()
-        Thread(target=self._poll_from_keyboard, args=()).start()
+        print("start called")
+        yield self.stream.connect((self._host, self._port))   # This returns a future so need @coroutine decorator
+        print("Connected to: ", self._host, self._port)
 
-    @coroutine
-    def _send_to_keyboard(self):
-        try:
-            while True:
-                msg_bytes = yield self.stream.read_until(b'\n')
-                msg = memoryview(msg_bytes[:-1]).tolist()
-                print("received ", msg_bytes, " and converted to ", msg)
-                msg_type = self._localmidi.get_msg_type(msg[0])
-                if msg_type == self._localmidi.ON_OFF:
-                    self._localmidi.MIDI_OUT_CONN.send_message([msg[0], msg[1] + 3, msg[2]])
-                else:
-                    self._localmidi.MIDI_OUT_CONN.send_message(msg)
+        Thread(target=self.listen_to_keyboard, args=()).start()
+        Thread(target=self.dispatch_to_server, args=()).start()
 
-        except StreamClosedError:
-            pass
+        Thread(target=self.listen_to_server, args=()).start()
+        Thread(target=self.dispatch_to_keyboard, args=()).start()
 
-    @coroutine
-    def _poll_from_keyboard(self):
-        """ Polls the connected MIDI device for incoming data, then sends it. """
+    def listen_to_keyboard(self):
+        """ Thread for listening to output from the keyboard and putting it into the keyboard listener queue. """
         while True:
-            msg, delta_time = self._localmidi.MIDI_IN_CONN.get_message()
-            if msg is not None and msg[0] is not self._localmidi.SYSEX_MSG:
-                print("Got note")
-                yield self.stream.write(bytes(msg) + '\n'.encode())
-                print("sent midi packet")
+            rtmidi_msg, time = self._localmidi.MIDI_IN_CONN.get_message()
+            if rtmidi_msg is not None and rtmidi_msg[0] is not self._localmidi.SYSEX_MSG:     # Do not store SYSEX messages
+                self.keyboard_listener.put(rtmidi_msg)
+                print(rtmidi_msg)
+
+    @coroutine
+    def dispatch_to_server(self):
+        """ Thread for retrieving notes from the keyboard listener queue and sending them to the server. """
+        while True:
+            if not self.keyboard_listener.empty():
+                rtmidi_msg = self.keyboard_listener.get()
+                yield self.stream.write(bytes(rtmidi_msg) + b'\n')     # Encode and concat with \n
+
+    @coroutine
+    def listen_to_server(self):
+        """ Thread for listening to output from the server and putting it into the server listener queue. """
+        while True:
+            msg_bytes = yield self.stream.read_until(delimiter=b'\n')
+            self.server_listener.put(msg_bytes)
+
+    def dispatch_to_keyboard(self):
+        """ Thread for retrieving input from tje server listener queue and sending it to the keyboard. """
+        while True:
+            if not self.server_listener.empty():
+                msg_bytes = self.server_listener.get()
+                msg = memoryview(msg_bytes[:-1]).tolist()  # Assuming contents of queue still have \n, strip them
+                self._localmidi.MIDI_OUT_CONN.send_message(msg)
 
     def on_disconnect(self):
         print("Disconnected client: ", self._host, self._port)
